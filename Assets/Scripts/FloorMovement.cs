1e3c87e70b4aed2bb57f2f7d7c7593e1c5e72a0e
Ethan Mahoney <60744824+emahoney929@users.noreply.github.com>
2022-11-28 12:32:43 -0800

    Floor movement for course 5

---
+++ Assets/Scripts/FloorMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorMovement : MonoBehaviour
+{
+    float timer = 0f;
+    float speed = 0.030f;
+    int phase = 0;
+    void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
+        if(timer>1f)
+        {
+            phase++;
+            phase %= 4;
+            timer = 0f;
+        }
+
+        switch(phase)
+        {
+            case 0:
+                transform.Translate(0f, speed * (1 - timer), 0f);
+                break;
+            case 1:
+                transform.Translate(0f, -speed * timer, 0f);
+                break;
+            case 2:
+                transform.Translate(0f, -speed * (1 - timer), 0f);
+                break;
+            case 3:
+                transform.Translate(0f, speed * timer, 0f);
+                break;
+        }
+    }
+}
