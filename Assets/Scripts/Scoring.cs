4bf80ef6d6592230e50c4a7b06e197130fb7932c
traviscarter24 <traviscarter@nevada.unr.edu>
2022-12-04 15:44:19 -0800

    Slight flixes

--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -6,21 +6,17 @@ using UnityEngine.UI;
 public class Scoring : MonoBehaviour
 {
     public int swings;
-    Text text;
-    int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         swings = 0;
-            text = transform.Find("Text").GetComponent<Text>();
+           
     }
 
     // Update is called once per frame
     void Update()
     { 
-        if (Input.GetMouseButtonDown(0)) {
-			score++;
-			text.text = "SCORE: "+score;
-		}
+        
 	}
 }
