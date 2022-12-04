9285b04bf32b970955e6fd6322f22fe870c19706
traviscarter24 <traviscarter@nevada.unr.edu>
2022-12-04 15:38:02 -0800

    Scoreboard in pause menu

--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scoring : MonoBehaviour
 {
     public int swings;
-
+    Text text;
+    int score = 0;
     // Start is called before the first frame update
     void Start()
     {
         swings = 0;
+            text = transform.Find("Text").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+    { 
+        if (Input.GetMouseButtonDown(0)) {
+			score++;
+			text.text = "SCORE: "+score;
+		}
+	}
 }
