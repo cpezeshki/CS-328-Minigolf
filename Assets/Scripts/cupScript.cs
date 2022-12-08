863698aace0726cc40e29909fd63401a5acb2f84
cpezeshki <66799492+cpezeshki@users.noreply.github.com>
2022-12-08 11:42:11 -0800

    Levels now blocked. Also updated to go to next cutscene

--- Assets/Scripts/cupScript.cs
+++ Assets/Scripts/cupScript.cs
@@ -22,7 +22,31 @@ public class cupScript : MonoBehaviour
     {
         if(other.name == "Golfball")
         {
-            SceneManager.LoadScene(levelSelect);
+            if(levelSelect == "Course2-Cutscene")
+            {
+                MainMenu.enableLevel2 = true;
+                SceneManager.LoadScene(levelSelect);
+            }
+            if(levelSelect == "Course3-Cutscene")
+            {
+                MainMenu.enableLevel3 = true;
+                SceneManager.LoadScene(levelSelect);
+            }
+            if(levelSelect == "Course4-Cutscene")
+            {
+                MainMenu.enableLevel4 = true;
+                SceneManager.LoadScene(levelSelect);
+            }
+            if(levelSelect == "Course5-Cutscene")
+            {
+                MainMenu.enableLevel5 = true;
+                SceneManager.LoadScene(levelSelect);
+            }
+            if(levelSelect == "Course6-Cutscene")
+            {
+                MainMenu.enableLevel6 = true;
+                SceneManager.LoadScene(levelSelect);
+            }
         }
     }
 }
