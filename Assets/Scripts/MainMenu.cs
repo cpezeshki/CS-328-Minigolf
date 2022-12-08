5d3aa875904573243f46fc107c3ce20394b023b1
cpezeshki <66799492+cpezeshki@users.noreply.github.com>
2022-12-08 12:01:20 -0800

    Updated script names

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
 
     public string firstLevel;
--- Assets/Scripts/cupScript.cs
+++ Assets/Scripts/cupScript.cs
@@ -24,27 +24,27 @@ public class cupScript : MonoBehaviour
         {
             if(levelSelect == "Course2-Cutscene")
             {
-                MainMenu.enableLevel2 = true;
+                LevelMenu.enableLevel2 = true;
                 SceneManager.LoadScene(levelSelect);
             }
             if(levelSelect == "Course3-Cutscene")
             {
-                MainMenu.enableLevel3 = true;
+                LevelMenu.enableLevel3 = true;
                 SceneManager.LoadScene(levelSelect);
             }
             if(levelSelect == "Course4-Cutscene")
             {
-                MainMenu.enableLevel4 = true;
+                LevelMenu.enableLevel4 = true;
                 SceneManager.LoadScene(levelSelect);
             }
             if(levelSelect == "Course5-Cutscene")
             {
-                MainMenu.enableLevel5 = true;
+                LevelMenu.enableLevel5 = true;
                 SceneManager.LoadScene(levelSelect);
             }
             if(levelSelect == "Course6-Cutscene")
             {
-                MainMenu.enableLevel6 = true;
+                LevelMenu.enableLevel6 = true;
                 SceneManager.LoadScene(levelSelect);
             }
         }
