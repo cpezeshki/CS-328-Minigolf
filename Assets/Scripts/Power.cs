384fb4bab1435bad077057d0e3df7a1687566d01
Ethan Mahoney <60744824+emahoney929@users.noreply.github.com>
2022-12-16 11:02:30 -0800

    Updated script for screen shake

--- Assets/Scripts/Power.cs
+++ Assets/Scripts/Power.cs
@@ -9,20 +9,29 @@ public class Power : MonoBehaviour
     public int minStrength = 1;
     public int maxStrength = 100;
     float strengthFactor = 0.1f;
+    public float shakeMagnitude = 0.05f;
+    public float shakeTime = 0.5f;
+    Vector3 cameraInitialPosition;
 
     float strength;
     float originalLevel;
+    float defaultShake;
 
     Rigidbody golfball;
     TextMeshPro display;
     Quaternion originalRotation;
+    public Camera mainCamera;
+
+    float cameraYPos;
 
     void Start ()
     {
         sensitivity = (maxStrength - minStrength) / 33;
         strength = minStrength;
-
+        cameraYPos = mainCamera.transform.position.y;
+        defaultShake = shakeMagnitude;
         golfball = GetComponentInParent <Rigidbody> ();
+
     }
 
     private void OnEnable()
@@ -31,7 +40,7 @@ public class Power : MonoBehaviour
         strength = minStrength;
     }
 
-    void Update ()
+    void LateUpdate ()
     {
         float strengthChange;
         float difference;
@@ -64,9 +73,36 @@ public class Power : MonoBehaviour
         
         else 
         {
+            ShakeIt();
             golfball.AddRelativeForce (strength * strengthFactor * Vector3.forward, ForceMode.Impulse);
-
+            //mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, , mainCamera.transform.position.z);
             gameObject.SetActive (false);
         }
     }
+
+    private void ShakeIt()
+	{
+		cameraInitialPosition = mainCamera.transform.position;
+        shakeMagnitude = defaultShake;
+        shakeMagnitude = shakeMagnitude * strength;
+        shakeMagnitude = shakeMagnitude / 80f;
+        Debug.Log(shakeMagnitude);
+		InvokeRepeating ("StartCameraShaking", 0f, 0.005f);
+		Invoke ("StopCameraShaking", shakeTime);
+	}
+
+	void StartCameraShaking()
+	{
+		float cameraShakingOffsetY = Random.value * shakeMagnitude * 2 - shakeMagnitude;
+		Vector3 cameraIntermadiatePosition = mainCamera.transform.position;
+		cameraIntermadiatePosition.y += cameraShakingOffsetY;
+		mainCamera.transform.position = cameraIntermadiatePosition;
+	}
+
+	void StopCameraShaking()
+	{
+        cameraInitialPosition.x = mainCamera.transform.position.x;
+        cameraInitialPosition.z = mainCamera.transform.position.z;
+		CancelInvoke ("StartCameraShaking");
+	}
 }
